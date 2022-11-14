4a32ad03937b50198c4882750543dc6a32006ad6
PatrikSzivos0528 <szivospatrikdavid@gmail.com>
2022-11-14 14:15:31 +0100

    main

--- 2022.11.08/pyhton feladatok/4.9/1.py
+++ 2022.11.08/pyhton feladatok/4.9/1.py
@@ -3,6 +3,9 @@ import turtle
 screen = turtle.Screen()
 teknos = turtle.Turtle()
 
+a = turtle.Screen()
+a.bgcolor("pink")
+
 teknos.forward(20)
 teknos.left(90)
 teknos.forward(20)
@@ -12,6 +15,51 @@ teknos.left(90)
 teknos.forward(20)
 teknos.left(90)
 teknos.forward(20)
+
+teknos.penup()
+teknos.forward(20)
+
+teknos.pendown()
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+
+
+teknos.penup()
+teknos.forward(20)
+teknos.pendown()
+
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+
+teknos.penup()
+teknos.forward(20)
+teknos.pendown()
+
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+
+
 
 
 screen.mainloop()
