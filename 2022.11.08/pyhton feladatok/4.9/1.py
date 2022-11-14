b45ff20a9a110021546cf90df028ce7e649aa52d
PatrikSzivos0528 <szivospatrikdavid@gmail.com>
2022-11-14 12:33:15 +0100

    main

---
+++ 2022.11.08/pyhton feladatok/4.9/1.py
@@ -0,0 +1,17 @@
+import turtle
+
+screen = turtle.Screen()
+teknos = turtle.Turtle()
+
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+teknos.left(90)
+teknos.forward(20)
+
+
+screen.mainloop()
