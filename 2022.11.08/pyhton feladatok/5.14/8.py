f49c5b92f368abbf754624bd5c5e4a2db6d0d7ce
PatrikSzivos0528 <szivospatrikdavid@gmail.com>
2022-11-15 16:53:41 +0100

    main

--- 2022.11.08/pyhton feladatok/5.14/8.py
+++ 2022.11.08/pyhton feladatok/5.14/8.py
@@ -2,6 +2,9 @@ import turtle
 
 Eszti = turtle.Turtle()
 
+    
+
+
 def rajzolj_oszlopot(t, magassag):
     t.begin_fill()
     t.left(90)
@@ -17,12 +20,21 @@ def rajzolj_oszlopot(t, magassag):
     t.penup()
     t.forward(10)
     t.pendown()
+
+def diagram(magassag):
+      color = ""
+      if magassag >= 200:
+         color = "red"
+      elif magassag < 200 and magassag >= 100:
+         color = "yellow"
+      elif magassag < 100:
+         color = "green"
     
 ablak = turtle.Screen()
 ablak.bgcolor("lightgreen")
 
 Eszti = turtle.Turtle()
-Eszti.color("blue", "red")
+Eszti.color()
 Eszti.pensize(3)
 
 xs = [48,117,200,240,160,260,220]
